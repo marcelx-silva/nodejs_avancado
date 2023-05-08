282e8a7ae9baec57a4bfb5822c5dd8efc54a73d6
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-05-08 17:16:17 -0300

    feat: add AuthenticationError class

--- src/domain/features/facebook-authentication.ts
+++ src/domain/features/facebook-authentication.ts
@@ -5,3 +5,10 @@ export interface FacebookAuthentication {
 type AccessToken = {
     accessToken: string
 }
+
+class AuthenticationError extends Error{
+    constructor() {
+        super("Authentication Failed");
+        this.name = "AuthenticationError"
+    }
+}
