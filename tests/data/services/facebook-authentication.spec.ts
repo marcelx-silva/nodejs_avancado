b6003b4903c70a5e0d2ba1922835da2a4c589ba8
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-05-08 21:45:51 -0300

    change test strategy to use jest mocks

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,32 +1,23 @@
 import {AuthenticationError} from "@/domain/errors/authentication";
-import {LoadFacebookUserAPI} from "@/data/contracts/apis";
 import {FacebookAuthenticationService} from "@/data/services";
-
-class LoadFacebookUserAPISpy implements LoadFacebookUserAPI {
-    token?:string
-    result = undefined
-    callsCount: number = 0
-    async loadUserByToken(params: LoadFacebookUserAPI.Params): Promise<LoadFacebookUserAPI.Result> {
-        this.token = params.token
-        this.callsCount++
-        return this.result;
-    }
-}
 
 describe("FacebookAuthenticationUseCase", () =>{
     it('should call user facebook api with correct params', async function () {
-        const loadFacebookUser = new LoadFacebookUserAPISpy();
+        const loadFacebookUser = {
+            loadUserByToken: jest.fn()
+        }
         const sut = new FacebookAuthenticationService(loadFacebookUser)
-        console.log(sut)
         await sut.perform({ token: 'any_token' })
 
-        expect(loadFacebookUser.token).toBe('any_token')
-        expect(loadFacebookUser.callsCount).toBe(1)
+        expect(loadFacebookUser.loadUserByToken).toHaveBeenCalledWith({ token: 'any_token'})
+        expect(loadFacebookUser.loadUserByToken).toHaveBeenCalledTimes(1)
     });
 
     it('should return authentication error when loadfacebookapi returns undefined', async function () {
-        const loadFacebookUser = new LoadFacebookUserAPISpy();
-        loadFacebookUser.result = undefined
+        const loadFacebookUser = {
+            loadUserByToken: jest.fn()
+        }
+        loadFacebookUser.loadUserByToken.mockResolvedValueOnce(undefined)
         const sut = new FacebookAuthenticationService(loadFacebookUser)
         const authResult = await sut.perform({ token: 'any_token' })
 
