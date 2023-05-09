7c14d7ac091a39ec7e6b892a24f347ca0ade3fd2
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-05-08 21:50:51 -0300

    add jest-mock-extended to mock interfaces easily

--- tests/data/services/facebook-authentication.spec.ts
+++ tests/data/services/facebook-authentication.spec.ts
@@ -1,11 +1,11 @@
 import {AuthenticationError} from "@/domain/errors/authentication";
 import {FacebookAuthenticationService} from "@/data/services";
+import {mock} from "jest-mock-extended";
+import {LoadFacebookUserAPI} from "@/data/contracts/apis";
 
 describe("FacebookAuthenticationUseCase", () =>{
     it('should call user facebook api with correct params', async function () {
-        const loadFacebookUser = {
-            loadUserByToken: jest.fn()
-        }
+        const loadFacebookUser = mock<LoadFacebookUserAPI>()
         const sut = new FacebookAuthenticationService(loadFacebookUser)
         await sut.perform({ token: 'any_token' })
 
@@ -14,9 +14,7 @@ describe("FacebookAuthenticationUseCase", () =>{
     });
 
     it('should return authentication error when loadfacebookapi returns undefined', async function () {
-        const loadFacebookUser = {
-            loadUserByToken: jest.fn()
-        }
+        const loadFacebookUser = mock<LoadFacebookUserAPI>()
         loadFacebookUser.loadUserByToken.mockResolvedValueOnce(undefined)
         const sut = new FacebookAuthenticationService(loadFacebookUser)
         const authResult = await sut.perform({ token: 'any_token' })
