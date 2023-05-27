7adf1c157ab2519a70f58668b70bf98d6ecb9ecc
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-05-27 20:15:08 -0300

    ensure access token expires in 30 minutes

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -2,4 +2,8 @@ export class AccessToken {
   constructor (private readonly value: string) {
     this.value = value
   }
+
+  static get expirationInMilliseconds (): number {
+    return 30 * 60 * 1000
+  }
 }
