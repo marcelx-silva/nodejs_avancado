1bc4c744974d79a6642dffb5b742dc144e18ebb2
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-06-17 16:23:06 -0300

    ensure AxiosClientHttp returns data on Success

--- tests/infra/http/Axios.spec.ts
+++ tests/infra/http/Axios.spec.ts
@@ -4,24 +4,42 @@ import axios from 'axios'
 jest.mock('axios')
 
 export class AxiosHttpClient {
-  async get (params: HttpGetClient.Params): Promise<void> {
-    return axios.get(params.url, { params: params.params })
+  async get (params: HttpGetClient.Params): Promise<any> {
+   const result = await axios.get(params.url, { params: params.params })
+    return result.data
   }
 }
 describe('AxiosHttpClient', () => {
+  let sut: AxiosHttpClient
+  let fakeAxios: jest.Mocked<typeof axios>
+  let url: string
+  let params: object
+  beforeAll(() => {
+    url = 'any_url'
+    params = {
+      any_param: 'any_value'
+    }
+    fakeAxios = axios as jest.Mocked<typeof axios>
+    fakeAxios.get.mockResolvedValue({
+      status: 200,
+      data: 'any_data'
+    })
+  })
+
+  beforeEach(() => {
+    sut = new AxiosHttpClient()
+  })
   describe('get', () => {
-    it('should call get with correct params', async () => {
-      const sut = new AxiosHttpClient()
-      const fakeAxios = axios as jest.Mocked<typeof axios>
-      await sut.get({
-        url: 'any_url',
-        params: {
-          any_param: 'any_value'
-        }
-      })
+    it('should call get with correct params', async () => {      
+      await sut.get({ url, params })
 
       expect(fakeAxios.get).toHaveBeenCalledWith('any_url', { params: { any_param: 'any_value' }}) 
       expect(fakeAxios.get).toHaveBeenCalledTimes(1)
+    })
+    it('should return data on success', async () => {      
+      const result = await sut.get({ url, params })
+
+      expect(result).toEqual('any_data') 
     })
   })
 })
