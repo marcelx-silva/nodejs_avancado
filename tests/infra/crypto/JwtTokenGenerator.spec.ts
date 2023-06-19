80fa84547e7cb2b34d9c3d1327c5e8b30172d89c
Thiago Marcelo <Thiago.silvamarcelo56@gmail.com>
2023-06-19 18:12:20 -0300

    make JwtTokengenerator implements TokenGenerator interface

--- tests/infra/crypto/JwtTokenGenerator.spec.ts
+++ tests/infra/crypto/JwtTokenGenerator.spec.ts
@@ -18,19 +18,19 @@ describe('JwtTokenGenerator', () =>{
   })
 
   it('should call sign with correct params', async () => {   
-    await sut.generateToken({ key: 'any_key', expires_in_ms: 1000 })
+    await sut.generate({ key: 'any_key', expires_in_ms: 1000 })
 
     expect(fakeJwt.sign).toHaveBeenCalledWith({key: 'any_key'},  'any_secret', { expiresIn: 1} )
   })
 
   it('should return a token', async ()=>{
-    const token = await sut.generateToken({ key: 'any_key', expires_in_ms: 1000 })
+    const token = await sut.generate({ key: 'any_key', expires_in_ms: 1000 })
     expect(token).toBe('any_token')
   })
 
   it('should rethrow if sign throws', async () => {
     fakeJwt.sign.mockImplementationOnce(() => { throw new Error('jwt_error') })
-    const promise = sut.generateToken({ key: 'any_key', expires_in_ms: 1000 })
+    const promise = sut.generate({ key: 'any_key', expires_in_ms: 1000 })
     expect(promise).rejects.toThrow(new Error('jwt_error'))
   })
 })
